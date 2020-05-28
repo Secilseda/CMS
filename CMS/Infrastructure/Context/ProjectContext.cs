ad7711c5e684a801eef4bf29cb9c46fa563c4adc
Seçil Seda Sağır <sedaa.secill18@outlook.com>
2020-05-28 12:50:39 +0300

    Installed  sqlserver  and tools packages, esabilish to connection to database

---
+++ CMS/Infrastructure/Context/ProjectContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.Infrastructure.Context
+{
+	public class ProjectContext:DbContext
+	{
+		public ProjectContext(DbContextOptions<ProjectContext>options):base(options)
+		{
+
+		}
+	}
+}
---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+
+1.Package Manager aracýlýðý ile aþaðýdaki paketler yüklenir.
+	1.1 Microsoft.EntityFrameworkCore.SqlServer
+	1.2 Microsoft.EntityFrameworkCore.Tools
+2.Infrastructure klasçrü oluþturulur.
+	2.1. Context klasörü oluþturulur.
+		2.1.1 ProjectContext.cs
